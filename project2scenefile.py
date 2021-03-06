0c13e1d8dbfd20dd6e3ca2e208cd6f5aac7ce6d3
Sterlo98 <79866961+Sterlo98@users.noreply.github.com>
2021-03-05 21:45:50 -0600

    Commit2/Filename Property Decorator
    Following the tutorial, I made the file name and it's paths properties.

--- project2scenefile.py
+++ project2scenefile.py
@@ -1,4 +1,5 @@
 class SceneFile(object):
+
     def __init__(self, folder_path, descriptor, task, ver, ext):
         self.folder_path = folder_path
         self.descriptor = descriptor
@@ -6,6 +7,19 @@ class SceneFile(object):
         self.ver = ver
         self.ext = ext
 
+    @property
+    def filename(self):
+        pattern = "{descriptor}_{task}_v{ver:03d}{ext}"
+        return pattern.format(descriptor=self.descriptor,
+                              task=self.task,
+                              ver=self.ver,
+                              ext=self.ext)
 
-scene_file = SceneFile("D:\\", "rocket", "model", "v001", ".ma")
-print(scene_file.descriptor)
+    @property
+    def path(self):
+        result = self.folder_path + "/" + self.filename
+        return result
+
+
+scene_file = SceneFile("D:\\", "rocket", "model", 1, ".ma")
+print(scene_file.path)
